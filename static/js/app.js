e4e383085b50b3b00d5210bd866bbad5d742efa6
webzhao <webzhao@gmail.com>
2017-07-06 12:30:59 +0800

    initial commit

---
+++ static/js/app.js
@@ -0,0 +1,120 @@
+{
+  const state = {
+    photos: undefined,
+    text: '',
+    background: ''
+  }
+
+  main()
+
+  /**
+   * generate some random color image
+   */
+  function getMonoColorImage(count) {
+    const canvas  = document.createElement('canvas')
+    const context = canvas.getContext('2d')
+    const baseColor = _ => Math.floor(Math.random() * 256)
+    const randomColor = _ => `rgb(${baseColor()}, ${baseColor()}, ${baseColor()})`
+    canvas.width  = 1
+    canvas.height = 1
+    return new Array(count).fill(1).map(_ => {
+      context.fillStyle = randomColor()
+      context.rect(0, 0, 1, 1)
+      context.fill()
+      return canvas.toDataURL()
+    })
+  }
+
+  /**
+   * prevent default for drag events
+   */
+  function prevent(e) {
+    e.stopPropagation()
+    e.preventDefault()
+  }
+
+  /**
+   * read file as data url and return promise
+   */
+  function getDataURL(file) {
+    const reader = new FileReader()
+    return new Promise((resolve, reject) => {
+      reader.readAsDataURL(file)
+      reader.addEventListener('load', _ => resolve(reader.result))
+    })
+  }
+
+  /**
+   * update photo count after drop
+   */
+  function updatePhotoCount(photos) {
+    document.querySelector('form p').innerHTML =
+      `Loaded ${photos.length} photos`
+  }
+
+  /**
+   * after file dropped
+   */
+  function onFilesDrop(files) {
+    const loadPhotos = Array.from(files)
+        .filter(file => /^image/.test(file.type))
+        .map(getDataURL)
+    Promise.all(loadPhotos).then(photos => {
+      state.photos = photos
+      updatePhotoCount(photos)
+    })
+  }
+
+  /**
+   * submit
+   */
+  function submit(e) {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    for (let [k, v] of formData.entries()) {
+      state[k] = v
+    }
+    generate()
+  }
+
+  /**
+   * generate photos
+   */
+  function generate() {
+    const photos = state.photos || getMonoColorImage(100)
+    const text = state.text || 'hi'
+    const background = state.background || '#000'
+    const glyphoto = new Glyphoto(photos, text, { background })
+    glyphoto.renderTo('#app')
+    document.querySelector('form').hidden = true
+  }
+
+  /**
+   * bind events
+   */
+  function bindEvents() {
+    // bind multiple events
+    const bind = (target, types, callback) => {
+      types.split(/\s+/).forEach(type => {
+        target.addEventListener(type, callback)
+      })
+    }
+    const body = document.body
+    const form = document.querySelector('form')
+    const activeClass = 'drag-active'
+    bind(body, 'dragover dragenter drop', prevent)
+    bind(body, 'dragenter', e => body.classList.add(activeClass))
+    bind(body, 'dragleave', e => body.classList.remove(activeClass))
+    bind(body, 'drop', e => body.classList.remove(activeClass))
+    bind(body, 'drop', e => onFilesDrop(e.dataTransfer.files))
+    bind(form, 'submit', submit)
+  }
+
+  /**
+   * entry function
+   */
+  function main() {
+    bindEvents()
+  }
+
+}
---
+++ static/js/glyphoto.js
@@ -0,0 +1,97 @@
+/**
+ * draw text with photos
+ */
+class Glyphoto {
+
+  /**
+   * init
+   * @param photos {Array} an list of photo urls
+   * @param text {String} text to draw
+   */
+  constructor(photos, text, {
+    fontFamily = 'sans-serif',
+    background = '#000'
+  } = {}) {
+    Object.assign(this, { photos, text, fontFamily, background })
+  }
+
+  /**
+   * get text matrix
+   */
+  _getTextMatrix(fontSize) {
+    const canvas  = document.createElement('canvas')
+    const context = canvas.getContext('2d')
+    canvas.width  = fontSize * this.text.length
+    canvas.height = fontSize
+    context.font  = `${fontSize}px ${this.fontFamily}`
+    const width   = Math.ceil(context.measureText(this.text).width)
+    const height  = fontSize
+    const baselinePos = 0.9
+    context.fillText(this.text, 0, fontSize * baselinePos)
+    const imageData = context.getImageData(0, 0, width, height).data
+    const threshold = 128
+    const pixels = imageData.filter((_, i) => i % 4 === 3).map(alpha => alpha > threshold)
+    return { width, height, pixels }
+  }
+
+  /**
+   * render in an container
+   */
+  renderTo(container) {
+    this.container = document.querySelector(container)
+    const width = this.container.scrollWidth
+    const height = this.container.scrollHeight || window.innerHeight
+    const layout = this.getLayout(width, height)
+    const fragment = this._generateNodes(this.photos.length)
+    const nodes = fragment.childNodes
+    layout.forEach((dot, i) => {
+      if (!nodes[i]) return
+      nodes[i].style.cssText = `
+        position: absolute;
+        width: ${dot.size}px;
+        height: ${dot.size}px;
+        left: ${dot.x}px;
+        top: ${dot.y}px;
+        background: url(${this.photos[i]}) no-repeat 0 0/cover;
+      `
+    })
+    this.container.style.background = this.background
+    this.container.appendChild(fragment)
+  }
+
+  /**
+   * generate nodes
+   */
+  _generateNodes(count) {
+    const fragment = new DocumentFragment()
+    for (let i = 0; i < count; i++) {
+      fragment.appendChild(document.createElement('div'))
+    }
+    return fragment
+  }
+
+  /**
+   * get size and position of each photo
+   */
+  getLayout(canvasWidth, canvasHeight) {
+    const probeFontSize = 50
+    const probeMatrix = this._getTextMatrix(probeFontSize)
+    const probeDotCount = probeMatrix.pixels.filter(hasColor => hasColor).length
+    const fontSize = Math.round(Math.sqrt(this.photos.length / probeDotCount) * probeFontSize)
+    const { width, height, pixels } = this._getTextMatrix(fontSize)
+    const dotSize = Math.floor(Math.min(canvasWidth / width, canvasHeight / height))
+    // pixels to dot position
+    return pixels.reduce((dots, hasColor, index) => {
+      if (!hasColor) return dots
+      const row = Math.floor(index / width)
+      const col = index % width
+      dots.push({
+        x: col * dotSize,
+        y: row * dotSize,
+        size: dotSize
+      })
+      return dots
+    }, [])
+  }
+
+}
